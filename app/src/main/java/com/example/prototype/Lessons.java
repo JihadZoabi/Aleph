b380bacf97892ed35e80362caa3ad7d11edd7505
Bar Friedrich <bar.friedrich@gmail.com>
2022-07-19 12:47:33 +0300

    Yes

---
+++ app/src/main/java/com/example/prototype/Lessons.java
@@ -0,0 +1,11 @@
+package com.example.prototype;
+
+import android.content.res.Resources;
+
+public class Lessons {
+    void get(Resources res) {
+        res.getStringArray(R.array.headings)
+    }
+}
+
+
