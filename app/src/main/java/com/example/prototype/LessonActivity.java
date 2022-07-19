7c07f552ba39b07e47052fd8a135cb2ddb09cf8a
Eyal Z <eyalzau@gmail.com>
2022-07-19 12:09:33 +0300

    arrays.xml

---
+++ app/src/main/java/com/example/prototype/LessonActivity.java
@@ -0,0 +1,24 @@
+package com.example.prototype;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class LessonActivity extends AppCompatActivity {
+
+
+    private int[] lessonArr;
+    private int lessonLength;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_lesson);
+
+        lessonLength = getIntent().getExtras().getInt("length");
+        lessonArr = new int[lessonLength];
+
+        String[] arr = getResources().getStringArray(R.array.question1);
+
+    }
+}
