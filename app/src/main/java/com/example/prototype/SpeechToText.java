91b6bdf46afd69518e73971f26cdda2a57ef6da7
Bar Friedrich <bar.friedrich@gmail.com>
2022-07-24 16:37:23 +0300

    Further cleanup

--- app/src/main/java/com/example/prototype/SpeechToText.java
+++ app/src/main/java/com/example/prototype/SpeechToText.java
@@ -5,25 +5,31 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
 import android.speech.RecognitionListener;
+import android.speech.RecognizerIntent;
 import android.speech.SpeechRecognizer;
 import android.util.Log;
 
 import java.util.List;
+import java.util.Locale;
 
 public class SpeechToText {
     private SpeechRecognizer s;
+    private Intent i;
     public SpeechToText(Context c) {
         s = SpeechRecognizer.createSpeechRecognizer(c);
         s.setRecognitionListener(new RL());
+        i = new Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH);
+        i.putExtra(RecognizerIntent.EXTRA_LANGUAGE_MODEL,
+                RecognizerIntent.LANGUAGE_MODEL_FREE_FORM);
+        i.putExtra(RecognizerIntent.EXTRA_LANGUAGE, Locale.forLanguageTag("he"));
     }
     public void start() {
-        s.startListening(new Intent());
+        s.startListening(i);
         final Handler h = new Handler();
         Log.d("STT", "start");
-        h.postDelayed(() -> {
-            s.stopListening();
-            Log.d("STT", "end");
-        }, 5000);
+    }
+    public void destroy() {
+        s.destroy();
     }
     private class RL implements RecognitionListener {
         public void onBeginningOfSpeech() {}
@@ -32,12 +38,14 @@ public class SpeechToText {
         public void onError(int error) {}
         public void onEvent(int eventType, Bundle params) {}
         public void onPartialResults(Bundle partialResults) {}
-        public void onReadyForSpeech(Bundle params) {}
+        public void onReadyForSpeech(Bundle params) {
+            Log.d("STT", "ready");
+        }
         public void onRmsChanged(float rmsdB) {}
         public void onResults(Bundle res) {
             List<String> data =
                     res.getStringArrayList(SpeechRecognizer.RESULTS_RECOGNITION);
-            Log.d("STT", data.toString());
+            Log.d("STT", data.get(0));
         }
     }
 }
