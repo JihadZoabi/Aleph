d59dd02bad18fd42145cad80f5e808d204286a8a
Jihad Zoabi <Jihadmagic@gmail.com>
2022-07-26 14:39:03 +0300

    Voice recognition.

--- app/src/main/java/com/example/prototype/fragments/HomeFragment.java
+++ app/src/main/java/com/example/prototype/fragments/HomeFragment.java
@@ -5,25 +5,40 @@ import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.os.Build;
 import android.os.Bundle;
-
+import static android.speech.SpeechRecognizer.*;
 import androidx.core.app.ActivityCompat;
 import androidx.core.content.ContextCompat;
 import androidx.fragment.app.Fragment;
 
+import android.speech.RecognitionListener;
+import android.speech.RecognizerIntent;
+import android.speech.SpeechRecognizer;
+import android.util.Log;
 import android.view.LayoutInflater;
+import android.view.MotionEvent;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.example.prototype.FoodActivity;
 import com.example.prototype.LessonActivity;
 import com.example.prototype.R;
+import com.example.prototype.SpeechToText;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Locale;
 
 public class HomeFragment extends Fragment {
     private View sportRect;
     private View foodRect;
     private View schoolRect;
+    private Button micButton;
+    private TextView resultsTextView;
     private final Integer RecordAudioRequestCode = 1;
+    private SpeechRecognizer speechRecognizer;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -38,12 +53,19 @@ public class HomeFragment extends Fragment {
         sportRect = v.findViewById(R.id.sportRect);
         foodRect = v.findViewById(R.id.foodRect);
         schoolRect = v.findViewById(R.id.SchoolRect);
+        micButton = v.findViewById(R.id.speak_button);
+        resultsTextView = v.findViewById(R.id.speak_results);
+
+        speechRecognizer = SpeechRecognizer.createSpeechRecognizer(getActivity());
+
+
         sportRect.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 startActivity(new Intent(getActivity(), LessonActivity.class));
             }
         });
+
         foodRect.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -58,16 +80,122 @@ public class HomeFragment extends Fragment {
             }
         });
 
-        if(ContextCompat.checkSelfPermission(getActivity(),Manifest.permission.RECORD_AUDIO) != PackageManager.PERMISSION_GRANTED){
+        if (ContextCompat.checkSelfPermission(getActivity(),Manifest.permission.RECORD_AUDIO) != PackageManager.PERMISSION_GRANTED){
             Toast.makeText(getActivity(), "NO PERMISSIONS", Toast.LENGTH_SHORT).show();
-          checkPermission();
+            checkPermission();
         }
         else {
             Toast.makeText(getActivity(), "PERMISSIONS", Toast.LENGTH_SHORT).show();
         }
 
+        speechRecognizer = SpeechRecognizer.createSpeechRecognizer(getActivity());
 
+        final Intent speechRecognizerIntent = new Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH);
+        speechRecognizerIntent.putExtra(RecognizerIntent.EXTRA_LANGUAGE_MODEL,RecognizerIntent.LANGUAGE_MODEL_FREE_FORM);
+        speechRecognizerIntent.putExtra(RecognizerIntent.EXTRA_LANGUAGE, Locale.getDefault());
+
+        speechRecognizer.setRecognitionListener(new RecognitionListener() {
+            @Override
+            public void onReadyForSpeech(Bundle bundle) {
+
+            }
+
+            @Override
+            public void onBeginningOfSpeech() {
+                resultsTextView.setText("Listening...");
+            }
+
+            @Override
+            public void onRmsChanged(float v) {
+                // Irrelevant
+            }
+
+            @Override
+            public void onBufferReceived(byte[] bytes) {
+
+            }
+
+            @Override
+            public void onEndOfSpeech() {
+                Log.d("SST","VOICE ENDED");
+            }
+
+            @Override
+            public void onError(int error) {
+                switch (error) {
+                    case ERROR_AUDIO:
+                        Log.d("SST_ERROR","Audio recording error.");
+                        break;
+                    case 14:
+                        //ERROR_CANNOT_CHECK_SUPPORT
+                        Log.d("SST_ERROR","The service does not allow to check for support.");
+                        break;
+                    case ERROR_CLIENT:
+                        Log.d("SST_ERROR","Other client side errors.");
+                        break;
+                    case ERROR_INSUFFICIENT_PERMISSIONS:
+                        Log.d("SST_ERROR","Insufficient permissions");
+                        break;
+                    case ERROR_LANGUAGE_NOT_SUPPORTED:
+                        Log.d("SST_ERROR","Requested language is not available to be used with the current version");
+                        break;
+                    case ERROR_LANGUAGE_UNAVAILABLE:
+                        Log.d("SST_ERROR","Requested language is supported, but not available currently");
+                        break;
+                    case ERROR_NETWORK:
+                        Log.d("SST_ERROR","Other network related errors.");
+                        break;
+                    case ERROR_TOO_MANY_REQUESTS:
+                        Log.d("SST_ERROR","Too many requests from the same client.");
+                        break;
+                    case ERROR_SERVER:
+                        Log.d("SST_ERROR","Server sends error status.");
+                        break;
+                    case ERROR_NO_MATCH:
+                        Log.d("SST_ERROR","No recognition result matched.");
+                        break;
+                    case ERROR_NETWORK_TIMEOUT:
+                        Log.d("SST_ERROR","Network operation timed out.");
+                        break;
+                }
+            }
+
+            @Override
+            public void onResults(Bundle bundle) {
+                ArrayList<String> data = bundle.getStringArrayList(SpeechRecognizer.RESULTS_RECOGNITION);
+                resultsTextView.setText(data.get(0));
+            }
+
+            @Override
+            public void onPartialResults(Bundle bundle) {
+
+            }
+
+            @Override
+            public void onEvent(int i, Bundle bundle) {
+
+            }
+        });
+
+        micButton.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View view, MotionEvent motionEvent) {
+                if (motionEvent.getAction() == MotionEvent.ACTION_UP){
+                    speechRecognizer.stopListening();
+                }
+                if (motionEvent.getAction() == MotionEvent.ACTION_DOWN){
+                    speechRecognizer.startListening(speechRecognizerIntent);
+                }
+                return false;
+            }
+        });
         return v;
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        speechRecognizer.destroy();
     }
 
 
--- app/src/main/java/com/example/prototype/fragments/LearnPhraseFragment.java
+++ app/src/main/java/com/example/prototype/fragments/LearnPhraseFragment.java
@@ -1,8 +1,25 @@
 package com.example.prototype.fragments;
 
+import static android.speech.SpeechRecognizer.ERROR_AUDIO;
+import static android.speech.SpeechRecognizer.ERROR_CLIENT;
+import static android.speech.SpeechRecognizer.ERROR_INSUFFICIENT_PERMISSIONS;
+import static android.speech.SpeechRecognizer.ERROR_LANGUAGE_NOT_SUPPORTED;
+import static android.speech.SpeechRecognizer.ERROR_LANGUAGE_UNAVAILABLE;
+import static android.speech.SpeechRecognizer.ERROR_NETWORK;
+import static android.speech.SpeechRecognizer.ERROR_NETWORK_TIMEOUT;
+import static android.speech.SpeechRecognizer.ERROR_NO_MATCH;
+import static android.speech.SpeechRecognizer.ERROR_SERVER;
+import static android.speech.SpeechRecognizer.ERROR_TOO_MANY_REQUESTS;
+
+import android.content.Intent;
 import android.media.Image;
 import android.os.Bundle;
+import android.speech.RecognitionListener;
+import android.speech.RecognizerIntent;
+import android.speech.SpeechRecognizer;
+import android.util.Log;
 import android.view.LayoutInflater;
+import android.view.MotionEvent;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
@@ -22,11 +39,17 @@ import com.squareup.picasso.Picasso;
 
 import org.w3c.dom.Text;
 
+import java.util.ArrayList;
+import java.util.Locale;
+
 public class LearnPhraseFragment extends Fragment {
     private TextView meaningInArabic;
     private ImageView pictureOfWord;
     private TextView HebrewWordInArabic;
     private TextView WordInHebrew;
+    private Button micButton;
+    private final Integer RecordAudioRequestCode = 1;
+    private SpeechRecognizer speechRecognizer;
     private final LearnPhrase lp;
     private final Lesson l;
 
@@ -50,8 +73,128 @@ public class LearnPhraseFragment extends Fragment {
         pictureOfWord = (ImageView) v.findViewById(R.id.pictureOfWord);
         HebrewWordInArabic = (TextView) v.findViewById(R.id.HebrewWordInArabic);
         WordInHebrew = (TextView) v.findViewById(R.id.WordInHebrew);
+        micButton = v.findViewById(R.id.micButton);
+        speechRecognizer = SpeechRecognizer.createSpeechRecognizer(getActivity());
+
+        final Intent speechRecognizerIntent = new Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH);
+        speechRecognizerIntent.putExtra(RecognizerIntent.EXTRA_LANGUAGE_MODEL,RecognizerIntent.LANGUAGE_MODEL_FREE_FORM);
+        speechRecognizerIntent.putExtra(RecognizerIntent.EXTRA_LANGUAGE, Locale.getDefault());
+
+        speechRecognizer.setRecognitionListener(new RecognitionListener() {
+            @Override
+            public void onReadyForSpeech(Bundle bundle) {
+
+            }
+
+            @Override
+            public void onBeginningOfSpeech() {
+                Log.d("SST","Listening...");
+            }
+
+            @Override
+            public void onRmsChanged(float v) {
+                // Irrelevant
+            }
+
+            @Override
+            public void onBufferReceived(byte[] bytes) {
+
+            }
+
+            @Override
+            public void onEndOfSpeech() {
+                Log.d("SST","VOICE ENDED");
+            }
+
+            @Override
+            public void onError(int error) {
+                switch (error) {
+                    case ERROR_AUDIO:
+                        Log.d("SST_ERROR","Audio recording error.");
+                        break;
+                    case 14:
+                        //ERROR_CANNOT_CHECK_SUPPORT
+                        Log.d("SST_ERROR","The service does not allow to check for support.");
+                        break;
+                    case ERROR_CLIENT:
+                        Log.d("SST_ERROR","Other client side errors.");
+                        break;
+                    case ERROR_INSUFFICIENT_PERMISSIONS:
+                        Log.d("SST_ERROR","Insufficient permissions");
+                        break;
+                    case ERROR_LANGUAGE_NOT_SUPPORTED:
+                        Log.d("SST_ERROR","Requested language is not available to be used with the current version");
+                        break;
+                    case ERROR_LANGUAGE_UNAVAILABLE:
+                        Log.d("SST_ERROR","Requested language is supported, but not available currently");
+                        break;
+                    case ERROR_NETWORK:
+                        Log.d("SST_ERROR","Other network related errors.");
+                        break;
+                    case ERROR_TOO_MANY_REQUESTS:
+                        Log.d("SST_ERROR","Too many requests from the same client.");
+                        break;
+                    case ERROR_SERVER:
+                        Log.d("SST_ERROR","Server sends error status.");
+                        break;
+                    case ERROR_NO_MATCH:
+                        Log.d("SST_ERROR","No recognition result matched.");
+                        break;
+                    case ERROR_NETWORK_TIMEOUT:
+                        Log.d("SST_ERROR","Network operation timed out.");
+                        break;
+                }
+            }
+
+            @Override
+            public void onResults(Bundle bundle) {
+                ArrayList<String> data = bundle.getStringArrayList(SpeechRecognizer.RESULTS_RECOGNITION);
+                HebrewWordInArabic.setText(data.get(0));
+                if (data.get(0).equals(lp.getAnswers()[2])) {
+                    Toast.makeText(getActivity(), "CORRECT", Toast.LENGTH_SHORT).show();
+                }
+                else {
+                    speechRecognizer.destroy();
+                    Toast.makeText(getActivity(), "Try Again", Toast.LENGTH_SHORT).show();
+                }
+                Log.d("SST",data.get(0));
+            }
+
+            @Override
+            public void onPartialResults(Bundle bundle) {
+
+            }
+
+            @Override
+            public void onEvent(int i, Bundle bundle) {
+
+            }
+        });
+
+        micButton.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View view, MotionEvent motionEvent) {
+                Log.d("SST","Hi");
+                if (motionEvent.getAction() == MotionEvent.ACTION_UP){
+                    speechRecognizer.destroy();
+                    speechRecognizer.stopListening();
+                }
+                if (motionEvent.getAction() == MotionEvent.ACTION_DOWN){
+                    speechRecognizer.startListening(speechRecognizerIntent);
+                }
+                return false;
+            }
+        });
+
+
         updateQuestion();
         return v;
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        speechRecognizer.destroy();
     }
 
     private void updateQuestion() {
