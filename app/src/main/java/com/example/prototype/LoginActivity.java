63ef2c51fbf1c9666a5bbc0618b3001088d96f2d
Bar Friedrich <bar.friedrich@gmail.com>
2022-07-29 01:12:04 +0300

    User and stuff

--- app/src/main/java/com/example/prototype/LoginActivity.java
+++ app/src/main/java/com/example/prototype/LoginActivity.java
@@ -8,59 +8,32 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.google.firebase.auth.FirebaseAuth;
-import com.google.firebase.auth.FirebaseUser;
-
 
 public class LoginActivity extends AppCompatActivity {
-
     private FirebaseAuth mAuth;
-    EditText emailText;
-    EditText passwordText;
-    Button loginBtn;
-
-    TextView toSignup;
+    private EditText emailText;
+    private EditText passwordText;
+    private Button loginBtn;
+    private TextView toSignup;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
-
         emailText = findViewById(R.id.email);
         passwordText = findViewById(R.id.password);
         loginBtn = findViewById(R.id.login_button);
         toSignup = findViewById(R.id.toSignup);
-
-        toSignup.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                startActivity(new Intent(LoginActivity.this, SignupActivity.class));
-            }
+        toSignup.setOnClickListener(view ->
+                startActivity(new Intent(LoginActivity.this, SignupActivity.class)));
+        loginBtn.setOnClickListener(view -> {
+            String email = emailText.getText().toString();
+            String password = passwordText.getText().toString();
+            User user = User.login(this, email, password,
+                    e -> Toast.makeText(this, e.toString(), Toast.LENGTH_SHORT).show());
         });
-
-
-
-        mAuth = FirebaseAuth.getInstance();
-
-
-
-
-        loginBtn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                String email = emailText.getText().toString();
-                String password = passwordText.getText().toString();
-                User user = new User(email, password, LoginActivity.this);
-                user.loginUser();
-                //loginUser(emailText.getText().toString(), passwordText.getText().toString());
-            }
-        });
-
-        mAuth = FirebaseAuth.getInstance();
-    }
-
-    private void updateUI(FirebaseUser user){
-        //TODO: make a useful updateUI function.
     }
 }
--- app/src/main/java/com/example/prototype/SignupActivity.java
+++ app/src/main/java/com/example/prototype/SignupActivity.java
@@ -2,58 +2,37 @@ package com.example.prototype;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.content.Intent;
 import android.os.Bundle;
-import android.text.TextUtils;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
-import com.google.android.gms.tasks.OnSuccessListener;
-import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
-import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 
-import java.util.Objects;
-
 public class SignupActivity extends AppCompatActivity {
-
     private EditText emailText;
     private EditText usernameText;
     private EditText passwordText;
     private Button signupBtn;
 
-    private FirebaseAuth mAuth;
-    private FirebaseDatabase db;
-    private DatabaseReference ref;
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_signup2);
-
         emailText = findViewById(R.id.email);
         usernameText = findViewById(R.id.username);
         passwordText = findViewById(R.id.password);
         signupBtn = findViewById(R.id.signup_button);
+        signupBtn.setOnClickListener(view -> {
+            String email = emailText.getText().toString();
+            String username = usernameText.getText().toString();
+            String password = passwordText.getText().toString();
 
-        mAuth = FirebaseAuth.getInstance();
-        db = FirebaseDatabase.getInstance();
-        ref = FirebaseDatabase.getInstance("https://spokenli-default-rtdb.europe-west1.firebasedatabase.app/").getReference("user/");
-
-        signupBtn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                String email = emailText.getText().toString();
-                String username = usernameText.getText().toString();
-                String password = passwordText.getText().toString();
-
-                if (email.length() >= 1 && password.length() >= 1) {
-                    User user = new User(email, password, SignupActivity.this);
-                    user.registerUser(username);
-                }
+            if (email.length() >= 1 && password.length() >= 1) {
+                User user = User.register(this, username, email, password,
+                        e -> Toast.makeText(this, e.toString(), Toast.LENGTH_SHORT).show());
             }
         });
     }
