e43c011eb044a7e2f17b106bdaf7549a7cdef52b
codoulingo1 <eyalzau@gmail.com>
2022-07-30 12:24:18 +0300

    SignsFragment

---
+++ app/src/main/java/com/example/prototype/fragments/SignsFragment.java
@@ -0,0 +1,34 @@
+package com.example.prototype.fragments;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.prototype.R;
+
+public class SignsFragment extends Fragment {
+
+    public SignsFragment() {
+        // Required empty public constructor
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View v = inflater.inflate(R.layout.fragment_signs, container, false);
+
+
+
+        return v;
+    }
+}
