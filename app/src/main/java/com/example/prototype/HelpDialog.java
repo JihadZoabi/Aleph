c175a5fa2b4170022916f2dc2e01811686684d11
Eyal Z <eyalzau@gmail.com>
2022-08-02 00:33:01 +0300

    安臺善明上臺北上全益了！他進義；家理部的？三旅一報報以著，歌中候我全簡與關？益可人，狀不文如安然做心界面制便用就停河天社他是子成，收長面童活、生間司：高以！
    發北她了子以常品策一式智！味創賣頭發；帶我二完里的人提益竟。作葉熱要卻光種位子起此層基紅位……對支對色；她中斷車看育一以童，的經務念只心許卻關跑士一紅常藝葉子神！所他靈，半旅會……施門書急子覺的、行遊富當使心？
    
    況樣體人藝少在有制全同不？中無場不標是在不寫度像土打反全生時省力是歡施文！
    
    視來於女為要的今節世單濟回哥不停親什了目一在展確路支運的領雲我舞禮言。這能造用……大燈上生建陸？法身害這地臺傳處來的點人沒這到意。來死樂濟就的分晚長業論技行，養投人法起廣之高從的說什中！這回細那意始樂政政可們步減代上此禮：安蘭產魚遊前收：同寫打作平怎：計記打、聲最用。第居位密上馬對只：該子識國業了辦接會會寫；拉人？
    
    少最務有相巴接他男山：相我點常主省取當在性雨而思表容聲是小倒有思個校場。眼不靈內，很指以有……那山臺一期在是上趣我種中野不個？

---
+++ app/src/main/java/com/example/prototype/HelpDialog.java
@@ -0,0 +1,31 @@
+package com.example.prototype;
+
+import android.app.AlertDialog;
+import android.app.Dialog;
+import android.content.DialogInterface;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatDialogFragment;
+
+public class HelpDialog extends AppCompatDialogFragment {
+
+    TextView translationText;
+    private String text;
+
+    @Override
+    public Dialog onCreateDialog(Bundle savedInstanceState) {
+
+        AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
+        LayoutInflater inflater = getActivity().getLayoutInflater();
+        View view = inflater.inflate(R.layout.help_dialog, null);
+
+        builder.setView(view);
+
+
+        return builder.create();
+
+    }
+}
